2f1c80f9b10a07fe727f0a6522dddd1e4c93e660
Santo Cariotti <dcariotti24@gmail.com>
2019-11-08 10:48:34 +0100

    Ex 12 Scrivere un metodo che preveda un parametro formale matrice quadrata di stringhe di dimensioni n × n, uno short k ed una stringa s, e restituisca un array di n puntatori a stringhe in cui il generico elemento di indice i contiene la i-esima stringa della diagonale secondaria se questa ha lunghezza maggiore o uguale a k e inizia con la stringa s, altrimenti la corrispondente stringa della diagonale principale.

---
+++ I_anno/Programmazione_1/ex12.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#define N 3
+using namespace std;
+
+string* func(string a[N][N], short k, string s) {
+    string* sn = new string[N];
+    vector<string> l;
+
+    // Diagonale secondaria
+    for(int i = N-1, j = 0; i >= 0; --i, ++j) {
+        l.push_back(a[i][j]);
+    } 
+
+    bool d2 = true;
+    for(auto const& i : l) {
+        if(i.length() < k || i.find(s) != 0) {
+            d2 = false;
+            break;
+        }
+    }
+
+    if(d2) {
+        for(int i = 0; i < N; ++i) {
+            sn[i] = l.at(i);
+        }
+    } else {
+        for(int i = 0; i < N; ++i) {
+            sn[i] = a[i][i];
+        }
+    }
+
+    return sn; 
+}
+
+int main() {
+    string a[N][N] = {
+        {"aab", "bbc", "dde"},
+        {"xyb", "bbc", "yz3"},
+        {"47x", "1y_", "23g"},
+    };
+    string* x = func(a, 3, "bb");
+
+    for(int i = 0; i < N; ++i)
+        cout << x[i] << ' ';
+    
+    cout << endl;
+
+    return 0;
+}
